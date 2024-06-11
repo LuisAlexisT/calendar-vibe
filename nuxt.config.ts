dcf0029a5d2d1a4a1d4be3d71b56cd1c54285be1
LuisAlexisT <alextepe27@gmail.com>
2024-06-11 00:55:06 -0600

    fix utils on primevue

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,12 +1,14 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
-export default defineNuxtConfig({
-  devtools: { enabled: true },
-  plugins: [
-    '~/plugins/primevue.js'
-  ],
+export default ({
   css: [
     'primevue/resources/themes/saga-blue/theme.css',
     'primevue/resources/primevue.min.css',
     'primeicons/primeicons.css'
-  ]
-})
+  ],
+  plugins: [
+    '~/plugins/primevue.js'
+  ],
+  build: {
+    transpile: ['primevue']
+  }
+});
--- plugins/primevue.js
+++ plugins/primevue.js
@@ -1,9 +1,27 @@
 // plugins/primevue.js
+// import { defineNuxtPlugin } from '#app';
+// import PrimeVue from 'primevue/config';
+// import Calendar from 'primevue/calendar';
+// import InputText from 'primevue/inputtext';
+// import SelectButton from 'primevue/selectbutton';
+// import 'primevue/resources/themes/saga-blue/theme.css';
+// import 'primevue/resources/primevue.min.css';
+// import 'primeicons/primeicons.css';
+
+// export default defineNuxtPlugin((nuxtApp) => {
+//   nuxtApp.vueApp.use(PrimeVue);
+//   nuxtApp.vueApp.component('Calendar', Calendar);
+//   nuxtApp.vueApp.component('InputText', InputText);
+//   nuxtApp.vueApp.component('SelectButton', SelectButton);
+// });
+
 import { defineNuxtPlugin } from '#app';
 import PrimeVue from 'primevue/config';
 import Calendar from 'primevue/calendar';
-import InputText from 'primevue/inputtext';
 import SelectButton from 'primevue/selectbutton';
+import InputText from 'primevue/inputtext';
+
+// Importa los estilos de PrimeVue y PrimeIcons
 import 'primevue/resources/themes/saga-blue/theme.css';
 import 'primevue/resources/primevue.min.css';
 import 'primeicons/primeicons.css';
@@ -11,6 +29,6 @@ import 'primeicons/primeicons.css';
 export default defineNuxtPlugin((nuxtApp) => {
   nuxtApp.vueApp.use(PrimeVue);
   nuxtApp.vueApp.component('Calendar', Calendar);
-  nuxtApp.vueApp.component('InputText', InputText);
   nuxtApp.vueApp.component('SelectButton', SelectButton);
+  nuxtApp.vueApp.component('InputText', InputText);
 });
